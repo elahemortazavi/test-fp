8d5661dddff39f1378ef01e729e72374a955eae1
elahemortazavi <emor5680@gmail.com>
2023-07-25 21:58:14 +0100

    home.css

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,6 +12,22 @@ const Home = () => {
           <li className="navListItem">TRAVEL CHECK</li>
           <li className="navListItem">ABOUT US</li>
         </ul>
+
+        <div className="container">
+          <div className="up">
+            <h1>Hub Planner</h1>
+            <p>"Your Attendance, Your Journey, One Click Away!"</p>
+          </div>
+          <div className="down">
+            <div className="downLeft">
+              <i className="topIcon fab fa-facebook-square"></i>
+              <i className="topIcon fab fa-instagram-square"></i>
+              <i className="topIcon fab fa-pinterest-square"></i>
+              <i className="topIcon fab fa-twitter-square"></i>
+            </div>
+            <img className='logo-img' src={Logo} alt='logo' />
+          </div>
+        </div>
       </div>
     </div>
   );
