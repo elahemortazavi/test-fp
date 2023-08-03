96245df3e3bfaea882504abcf243e733c30f822e
elahemortazavi <emor5680@gmail.com>
2023-08-03 11:47:48 +0100

    login js & css

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@
 import './App.css';
 import Calendar from './pages/Calendar';
 import Main from './pages/Main';
+import Login from "./pages/Login";
+import Form from "./pages/form";
 import Attendance from "./pages/Attendance";
 import TravelCheck from "./pages/Travel";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
