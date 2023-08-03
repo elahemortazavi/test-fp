834df73d467b4894c418803e0bd047cea9c1d87c
elahemortazavi <emor5680@gmail.com>
2023-08-03 01:16:00 +0100

    css

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Logo from "./img/CYF-logo2.png";
+import Logo1 from "./img/cyfLogo1.png"
 import { Link } from "react-router-dom";
 import "./main.css";
 
 const Main = () => {
   return (
-    <div>
+    <>
       <div className="navbar">
         <ul className="navList">
           <li className="navListItem">
@@ -18,28 +19,36 @@ const Main = () => {
               CYF CALENDAR
             </Link>
           </li>
-          <li className="navListItem">ATTENDANCE</li>
-          <li className="navListItem">TRAVEL CHECK</li>
-          
+          <li className="navListItem">
+            <Link className="link" to="/attendance">
+              ATTENDANCE
+            </Link>
+          </li>
+          <li className="navListItem">
+            <Link className="link" to="/travel">
+              TRAVEL CHECK
+            </Link>
+          </li>
         </ul>
+        <img className="logo1" src={Logo1} alt="logo" />
+      </div>
 
-        <div className="container">
-          <div className="up">
-            <h1>Hub Planner</h1>
-            <p>"Your Attendance, Your Journey, One Click Away!"</p>
+      <div className="container">
+        <div className="up">
+          <h1>Hub Planner</h1>
+          <p>"Your Attendance, Your Journey, One Click Away!"</p>
+        </div>
+        <div className="down">
+          <div className="downLeft">
+            <i className="topIcon fab fa-facebook-square"></i>
+            <i className="topIcon fab fa-instagram-square"></i>
+            <i className="topIcon fab fa-pinterest-square"></i>
+            <i className="topIcon fab fa-twitter-square"></i>
           </div>
-          <div className="down">
-            <div className="downLeft">
-              <i className="topIcon fab fa-facebook-square"></i>
-              <i className="topIcon fab fa-instagram-square"></i>
-              <i className="topIcon fab fa-pinterest-square"></i>
-              <i className="topIcon fab fa-twitter-square"></i>
-            </div>
-            <img className="logo-img" src={Logo} alt="logo" />
-          </div>
+          <img className="logo-img" src={Logo} alt="logo" />
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
