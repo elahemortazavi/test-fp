3329b5af3f0dcdd3b0f0c57909b262438cee2829
elahemortazavi <emor5680@gmail.com>
2023-08-09 23:10:38 +0100

    Update Attendance.js

--- client/src/pages/Attendance.js
+++ client/src/pages/Attendance.js
@@ -112,11 +112,10 @@
 
 
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./attendance.css";
 import Logo from "./img/cyfLogo1.png";
-import { useEffect, useState } from "react";
 
 const Attendance = () => {
   const [userData, setUserData] = useState({ name: "", role: "" });
@@ -127,13 +126,27 @@ const Attendance = () => {
   const [onlineVolunteers, setOnlineVolunteers] = useState([]);
   const [onlineTrainees, setOnlineTrainees] = useState([]);
 
-    useEffect(() => {
-      fetchUserData();
-      fetchAttendanceData();
-    }, []);
+  useEffect(() => {
+    fetchUserData();
+    fetchAttendanceData();
+  }, []);
 
-    const fetchUserData = async () => {
-    // ... (existing fetchUserData code)
+  const fetchUserData = async () => {
+    try {
+      const response = await fetch("/fetch-user-data", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setUserData(data);
+      }
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
   };
 
   const fetchAttendanceData = async () => {
@@ -147,7 +160,6 @@ const Attendance = () => {
 
       if (response.ok) {
         const data = await response.json();
-        // Filter and set data in the appropriate state variables
         setInPersonVolunteers(data.inPersonVolunteers);
         setInPersonTrainees(data.inPersonTrainees);
         setOnlineVolunteers(data.onlineVolunteers);
@@ -158,7 +170,138 @@ const Attendance = () => {
     }
   };
 
-  // ... (handleSubmit and return JSX)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("/submit-attendance", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: userData.name,
+          role: userData.role,
+          date,
+          attendanceType,
+        }),
+      });
+
+      if (response.ok) {
+        // Refresh or update the displayed data after successful submission
+        // You might want to call a function to update the displayed attendance data here
+      }
+    } catch (error) {
+      console.error("Error submitting attendance:", error);
+    }
+  };
+
+  return (
+    <div className="top-container">
+      <div className="navbar">
+        <ul className="navList">
+          <li className="navListItem">
+            <Link className="link" to="/main">
+              MAIN
+            </Link>
+          </li>
+
+          <li className="navListItem">
+            <Link className="link" to="/calendar">
+              CYF CALENDAR
+            </Link>
+          </li>
+
+          <li className="navListItem">
+            <Link className="link" to="/attendance">
+              ATTENDANCE
+            </Link>
+          </li>
+
+          <li className="navListItem">
+            <Link className="link" to="/travel">
+              TRAVEL CHECK
+            </Link>
+          </li>
+        </ul>
+        <img className="logo-img" src={Logo} alt="logo" />
+      </div>
+      
+      <div className="middle-container">
+        <form className="middle-container" onSubmit={handleSubmit}>
+          <div className="input-container">
+            <input type="text" id="name-input" placeholder="Name"></input>
+          </div>
+
+          <div className="input-container">
+            <input type="text" id="role-input" placeholder="Role"></input>
+          </div>
+
+          <div className="input-container">
+            <input type="date" id="date-input" placeholder="Date"></input>
+          </div>
+
+          <div className="attendance-select">
+            <select className="select-container" id="attendanceType">
+              <option>Attendance</option>
+              <option value="in-person">In-Person</option>
+              <option value="remote">Online</option>
+              <option value="not-attend">Not-Attend</option>
+            </select>
+          </div>
+
+          <button type="submit" className="submit-btn">
+            Submit
+          </button>
+        </form>
+      </div>
+      <div className="container">
+        <div className="section">
+          <h2>In-Person</h2>
+          <div className="columns">
+            <div className="column">
+              <h3>Volunteer</h3>
+              <ul className="list">
+                {inPersonVolunteers.map((user) => (
+                  <li key={user.name}>{user.name}</li>
+                ))}
+              </ul>
+            </div>
+            <div className="column">
+              <h3>Trainee</h3>
+              <ul className="list">
+                {inPersonTrainees.map((user) => (
+                  <li key={user.name}>{user.name}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="section">
+          <h2>Online</h2>
+          <div className="columns">
+            <div className="column">
+              <h3>Volunteer</h3>
+              <ul className="list">
+                {onlineVolunteers.map((user) => (
+                  <li key={user.name}>{user.name}</li>
+                ))}
+              </ul>
+            </div>
+            <div className="column">
+              <h3>Trainee</h3>
+              <ul className="list">
+                {onlineTrainees.map((user) => (
+                  <li key={user.name}>{user.name}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Attendance;
+
