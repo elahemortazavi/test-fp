ed89554af6e6de65c13971716e97e09e5568d21a
elahemortazavi <emor5680@gmail.com>
2023-08-10 14:04:50 +0100

    server.js updated

--- server/server.js
+++ server/server.js
@@ -1,20 +1,43 @@
 import express from "express";
 import cors from "cors"; // Import the cors package
-import pool from "./db"; // Import your database connection
+// import { Pool } from "pg"; // Import your database connection
+import pkg from "pg";
+const { Pool } = pkg;
+ 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors()); // Use cors middleware
-
 app.use(express.json()); // Parse JSON requests
+
+
+import dotenv from "dotenv";
+dotenv.config();
+
+
+dotenv.config();
+
+
+
+const dbPool = new Pool({
+  connectionString: process.env.DB_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+
+
+
+
 
 // Fetch user data
 app.get("/fetch-user-data", async (_, res) => {
   try {
     // Replace 'users' with your actual table name
-    const user = await pool.query(
-      "SELECT name, role FROM users WHERE id = $1",
+    const user = await dbPool.query(
+      "SELECT name, role FROM users2 WHERE id = $1",
       [userId]
     ); // userId should be obtained from authentication
     res.json(user.rows[0]);
@@ -27,8 +50,8 @@ app.get("/fetch-user-data", async (_, res) => {
 // Fetch attendance data
 app.get("/fetch-attendance-data", async (_, res) => {
   try {
-    const attendanceData = await pool.query(
-      "SELECT name, role, attendance_type FROM Attendance INNER JOIN users ON Attendance.userID = users.id"
+    const attendanceData = await dbPool.query(
+      "SELECT name, role, attendance_type FROM Attendance INNER JOIN users2 ON Attendance.userID = users2.id"
     );
 
     const inPersonVolunteers = attendanceData.rows.filter(
@@ -65,7 +88,7 @@ app.post("/submit-attendance", async (req, res) => {
 
   try {
     // Replace 'Attendance' with your actual table name
-    const newAttendance = await pool.query(
+    const newAttendance = await dbPool.query(
       "INSERT INTO Attendance (userID, name, role, date, attendance_type) VALUES ($1, $2, $3, $4, $5) RETURNING *",
       [userId, name, role, date, attendanceType] // userId should be obtained from authentication
     );
