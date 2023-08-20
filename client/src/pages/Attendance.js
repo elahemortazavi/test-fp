6f8075b0a3a4f9ef7f4b1f83ef428f3e1577cafb
elahemortazavi <emor5680@gmail.com>
2023-08-20 16:02:24 +0100

    attendance.js updated

--- client/src/pages/Attendance.js
+++ client/src/pages/Attendance.js
@@ -1,293 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import { Link } from "react-router-dom";
-// import "./attendance.css";
-// import Logo from "./img/cyfLogo1.png";
-// import moment from "moment";
-
-// const Attendance = () => {
-//   const user = JSON.parse(localStorage.getItem("user"));
-//   console.log(user);
-//   const [name, setName] = useState(user.name);
-//   const [role, setRole] = useState(user.role);
-//   const [userID, setUserID] = useState(user.id);
-//   const [date, setDate] = useState("");
-//   const [attendanceType, setAttendanceType] = useState("");
-//   const [inPersonVolunteers, setInPersonVolunteers] = useState();
-//   const [inPersonTrainees, setInPersonTrainees] = useState();
-//   const [onlineVolunteers, setOnlineVolunteers] = useState();
-//   const [onlineTrainees, setOnlineTrainees] = useState();
-
-//   useEffect(() => {
-//     fetchAttendanceData();
-//   }, []);
-
-//   const fetchAttendanceData = async () => {
-//     try {
-//       const response = await fetch("api/fetch-attendance-data", {
-//         method: "GET",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//       });
-//       const data = await response.json();
-//       console.log(data);
-//       // const inpersonVol = data.filter(
-//       // 	(item) =>
-//       // 		item.role.includes("volunteer") &&
-//       // 		item.attendance_type === "in-person"
-//       // );
-//       // setInPersonVolunteers(inpersonVol);
-//       // const onlineVol = data.filter(
-//       // 	(item) =>
-//       // 		item.role.includes("volunteer") &&
-//       // 		item.attendance_type === "remote"
-//       // );
-//       // setOnlineVolunteers(onlineVol);
-//       // const inpersonTrain = data.filter(
-//       // 	(item) => item.attendance_type === "in-person"
-//       // );
-//       // setInPersonTrainees(inpersonTrain);
-//       // const onlineTrain = data.filter(
-//       // 	(item) => item.attendance_type === "remote"
-//       // );
-//       // setOnlineTrainees(onlineTrain);
-//       // Separate volunteers and trainees based on attendance type
-//       const volunteers = data.filter((item) => item.role.includes("volunteer"));
-//       const trainees = data.filter((item) => item.role.includes("trainee"));
-
-//       // Further separate volunteers and trainees based on attendance type
-//       const inPersonVolunteers = volunteers.filter(
-//         (item) => item.attendance_type === "in-person"
-//       );
-//       const onlineVolunteers = volunteers.filter(
-//         (item) => item.attendance_type === "remote"
-//       );
-//       const inPersonTrainees = trainees.filter(
-//         (item) => item.attendance_type === "in-person"
-//       );
-//       const onlineTrainees = trainees.filter(
-//         (item) => item.attendance_type === "remote"
-//       );
-
-//       setInPersonVolunteers(inPersonVolunteers);
-//       setOnlineVolunteers(onlineVolunteers);
-//       setInPersonTrainees(inPersonTrainees);
-//       setOnlineTrainees(onlineTrainees);
-//     } catch (error) {
-//       console.error("Error fetching attendance data:", error);
-//     }
-//   };
-//   //old handlesubmit://
-//   // const handleSubmit = async (e) => {
-//   // 		e.preventDefault();
-//   // 		try {
-//   // 			const response = await fetch("api/submit-attendance", {
-//   // 				method: "POST",
-//   // 				headers: {
-//   // 					"Content-Type": "application/json",
-//   // 				},
-//   // 				body: JSON.stringify({
-//   // 					userID,
-//   // 					name,
-//   // 					role,
-//   // 					date,
-//   // 					attendanceType,
-//   // 				}),
-//   // 			});
-//   // 			fetchAttendanceData();
-//   // 		} catch (error) {
-//   // 			console.error("Error submitting attendance:", error);
-//   // 		}
-//   // 	};
-
-//   //new handleSubmit://
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-
-//     try {
-//       // Check if the user has already submitted attendance
-//       const existingAttendance = await fetch("api/check-attendance", {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({
-//           userID,
-//           date,
-//         }),
-//       });
-
-//       if (existingAttendance.ok) {
-//         // Delete the user's previous submission
-//         await fetch("api/delete-attendance", {
-//           method: "POST",
-//           headers: {
-//             "Content-Type": "application/json",
-//           },
-//           body: JSON.stringify({
-//             userID,
-//             date,
-//           }),
-//         });
-//       }
-
-//       // Submit the new attendance
-//       const response = await fetch("api/submit-attendance", {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({
-//           userID,
-//           name,
-//           role,
-//           date,
-//           attendanceType,
-//         }),
-//       });
-
-//       if (response.ok) {
-//         fetchAttendanceData(); // Refresh the attendance data
-//       }
-//     } catch (error) {
-//       console.error("Error submitting attendance:", error);
-//     }
-//   };
-
-//   return (
-//     <div className="top-container">
-//       <div className="navbar">
-//         <ul className="navList">
-//           <li className="navListItem">
-//             <Link className="link" to="/main">
-//               MAIN
-//             </Link>
-//           </li>
-//           <li className="navListItem">
-//             <Link className="link" to="/calendar">
-//               CYF CALENDAR
-//             </Link>
-//           </li>
-//           <li className="navListItem">
-//             <Link className="link" to="/attendance">
-//               ATTENDANCE
-//             </Link>
-//           </li>
-//           <li className="navListItem">
-//             <Link className="link" to="/travel">
-//               TRAVEL CHECK
-//             </Link>
-//           </li>
-//         </ul>
-//         <img className="logo-img" src={Logo} alt="logo" />
-//       </div>
-//       <div className="middle-container">
-//         <form className="middle-container" onSubmit={handleSubmit}>
-//           <div className="input-container">
-//             <input type="text" value={name} />
-//           </div>
-//           <div className="input-container">
-//             <input type="text" value={role} />
-//           </div>
-//           <div className="input-container">
-//             <input
-//               type="date"
-//               id="date-input"
-//               placeholder="Date"
-//               onChange={(e) => setDate(e.target.value)}
-//               value={date}
-//             ></input>
-//           </div>
-//           <div className="attendance-select">
-//             <select
-//               className="select-container"
-//               id="attendanceType"
-//               onChange={(e) => setAttendanceType(e.target.value)}
-//             >
-//               <option>Attendance</option>
-//               <option value="in-person">In-Person</option>
-//               <option value="remote">Online</option>
-//               {/* <option value="not-attend">Not-Attend</option> */}
-//             </select>
-//           </div>
-//           <button type="submit" className="submit-btn">
-//             Submit
-//           </button>
-//         </form>
-//       </div>
-//       <div className="container">
-//         <div className="section">
-//           <h2>In-Person</h2>
-//           <div className="columns">
-//             <div className="column">
-//               <h3>Volunteer({inPersonVolunteers?.length || 0})</h3>
-//               <ul className="list">
-//                 {inPersonVolunteers
-//                   ?.sort((a, b) => b.date.localeCompare(a.date))
-//                   .map((user) => (
-//                     <li key={user.name}>
-//                       {user.name}(
-//                       <span>{moment(user.date).format("Do MMMM YYYY")}</span>)
-//                     </li>
-//                   ))}
-//               </ul>
-//             </div>
-//             <div className="column">
-//               <h3>Trainee({inPersonTrainees?.length || 0})</h3>
-//               <ul className="list">
-//                 {inPersonTrainees
-//                   ?.sort((a, b) => b.date.localeCompare(a.date))
-//                   .map((user) => (
-//                     <li key={user.name}>
-//                       {user.name}(
-//                       <span>{moment(user.date).format("Do MMMM YYYY")}</span>)
-//                     </li>
-//                   ))}
-//               </ul>
-//             </div>
-//           </div>
-//         </div>
-//         <div className="section">
-//           <h2>Online</h2>
-//           <div className="columns">
-//             <div className="column">
-//               <h3>Volunteer({onlineVolunteers?.length || 0})</h3>
-//               <ul className="list">
-//                 {onlineVolunteers
-//                   ?.sort((a, b) => b.date.localeCompare(a.date))
-//                   .map((user) => (
-//                     <li key={user.name}>
-//                       {user.name}(
-//                       <span>{moment(user.date).format("Do MMMM YYYY")}</span>)
-//                     </li>
-//                   ))}
-//               </ul>
-//             </div>
-//             <div className="column">
-//               <h3>Trainee({onlineTrainees?.length || 0})</h3>
-//               <ul className="list">
-//                 {onlineTrainees
-//                   ?.sort((a, b) => b.date.localeCompare(a.date))
-//                   .map((user) => (
-//                     <li key={user.name}>
-//                       {user.name}(
-//                       <span>{moment(user.date).format("Do MMMM YYYY")}</span>)
-//                     </li>
-//                   ))}
-//               </ul>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-// export default Attendance;
-
-
-
-
-
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./attendance.css";
@@ -307,6 +17,18 @@ const Attendance = () => {
   const [onlineVolunteers, setOnlineVolunteers] = useState();
   const [onlineTrainees, setOnlineTrainees] = useState();
   const [submissionStatus, setSubmissionStatus] = useState(null);
+  const [dateError, setDateError] = useState(false);
+  const [attendanceTypeError, setAttendanceTypeError] = useState(false);
+
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+    setDateError(false);
+  };
+
+  const handleAttendanceTypeChange = (e) => {
+    setAttendanceType(e.target.value);
+    setAttendanceTypeError(false);
+  };
 
   // const [data, setData] = useState([]);
 
@@ -443,6 +165,21 @@ const Attendance = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    // Validate inputs before submitting
+    if (!date) {
+      setDateError(true);
+      return;
+    } else {
+      setDateError(false);
+    }
+
+    if (!attendanceType) {
+      setAttendanceTypeError(true);
+      return;
+    } else {
+      setAttendanceTypeError(false);
+    }
+
     try {
       // Check if the user has already submitted attendance
       const existingAttendance = await fetch("api/check-attendance", {
@@ -488,6 +225,9 @@ const Attendance = () => {
       if (response.ok) {
         fetchAttendanceData(); // Refresh the attendance data
         setSubmissionStatus("submitted");
+        // Clear input fields after successful submission
+        setDate("");
+        setAttendanceType("");
       }
     } catch (error) {
       console.error("Error submitting attendance:", error);
@@ -534,21 +274,38 @@ const Attendance = () => {
               type="date"
               id="date-input"
               placeholder="Date"
-              onChange={(e) => setDate(e.target.value)}
+              // onChange={(e) => setDate(e.target.value)}
+              onChange={handleDateChange}
               value={date}
-            ></input>
+              className={dateError ? "error" : ""}
+            />
+            {dateError && (
+              <p className="validation-error">Please select a date.</p>
+            )}
           </div>
+
           <div className="attendance-select">
             <select
-              className="select-container"
+              // className="select-container"
+              className={`select-container ${
+                attendanceTypeError ? "error" : ""
+              }`}
               id="attendanceType"
-              onChange={(e) => setAttendanceType(e.target.value)}
+              // onChange={(e) => setAttendanceType(e.target.value)}
+              onChange={handleAttendanceTypeChange}
+              value={attendanceType}
             >
-              <option>Attendance</option>
+              <option value="" disabled selected>
+                Attendance
+              </option>
               <option value="in-person">In-Person</option>
               <option value="remote">Online</option>
-              {/* <option value="not-attend">Not-Attend</option> */}
             </select>
+            {attendanceTypeError && (
+              <p className="validation-error">
+                Please select an attendance type.
+              </p>
+            )}
           </div>
           <div className="submit-container">
             <button type="submit" className="submit-btn">
@@ -656,7 +413,16 @@ const Attendance = () => {
               <h2>In-Person</h2>
               <div className="columns">
                 <div className="column">
-                  <h3>Volunteer({attendeesBySelectedDate?.length || 0})</h3>
+                  <h3>
+                    {/* Volunteer({attendeesBySelectedDate?.length || 0}) */}
+                    Volunteer(
+                    {attendeesBySelectedDate?.filter(
+                      (user) =>
+                        user.attendance_type === "in-person" &&
+                        user.role.includes("volunteer")
+                    ).length || 0}
+                    )
+                  </h3>
                   <ul className="list">
                     {attendeesBySelectedDate
                       ?.filter(
@@ -676,7 +442,16 @@ const Attendance = () => {
                   </ul>
                 </div>
                 <div className="column">
-                  <h3>Trainee({attendeesBySelectedDate?.length || 0})</h3>
+                  <h3>
+                    {/* Trainee({attendeesBySelectedDate?.length || 0}) */}
+                    Trainee(
+                    {attendeesBySelectedDate?.filter(
+                      (user) =>
+                        user.attendance_type === "in-person" &&
+                        user.role.includes("trainee")
+                    ).length || 0}
+                    )
+                  </h3>
                   <ul className="list">
                     {attendeesBySelectedDate
                       ?.filter(
@@ -701,7 +476,16 @@ const Attendance = () => {
               <h2>Online</h2>
               <div className="columns">
                 <div className="column">
-                  <h3>Volunteer({attendeesBySelectedDate?.length || 0})</h3>
+                  <h3>
+                    {/* Volunteer({attendeesBySelectedDate?.length || 0}) */}
+                    Volunteer(
+                    {attendeesBySelectedDate?.filter(
+                      (user) =>
+                        user.attendance_type === "remote" &&
+                        user.role.includes("volunteer")
+                    ).length || 0}
+                    )
+                  </h3>
                   <ul className="list">
                     {attendeesBySelectedDate
                       ?.filter(
@@ -721,7 +505,16 @@ const Attendance = () => {
                   </ul>
                 </div>
                 <div className="column">
-                  <h3>Trainee({attendeesBySelectedDate?.length || 0})</h3>
+                  <h3>
+                    {/* Trainee({attendeesBySelectedDate?.length || 0}) */}
+                    Trainee(
+                    {attendeesBySelectedDate?.filter(
+                      (user) =>
+                        user.attendance_type === "remote" &&
+                        user.role.includes("trainee")
+                    ).length || 0}
+                    )
+                  </h3>
                   <ul className="list">
                     {attendeesBySelectedDate
                       ?.filter(
